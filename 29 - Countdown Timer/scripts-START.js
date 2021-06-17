93c97a3c377f9a4df28bd4cd491e6aaadb97808e
handsomemolly <ryan.kendig@gmail.com>
2021-06-17 14:44:27 -0700

    countdown timer working

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,15 @@
+let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  //   console.log(now, then);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+    console.log(secondsLeft);
+  }, 1000);
+}
